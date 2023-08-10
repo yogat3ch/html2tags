973f56c8708379d955ff1210a739f94143321063
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2023-08-09 21:51:53 -0400

    bugfixes and docs for `parse_html`

--- R/html.R
+++ R/html.R
@@ -1,10 +1,22 @@
+#' Convert arbitrary HTML to htmltools tags
+#'
+#' @param html \code{chr} HTML or path to html file
+#'
+#' @return \code{shiny.tag.list}
+#' @export
+#' @importFrom htmltools tags
+#' @examples
+#' parse_html('<div id = "test"></div>')
 parse_html <- function(html) {
+  if (file.exists(html))
+    html <- paste0(readLines(html), collapse = "\n")
   ctx <- V8::v8()
   ctx$source("https://raw.githubusercontent.com/stla/html2R/master/inst/www/himalaya.js")
   ctx$assign("html", html)
   ctx$eval("const json = himalaya.parse(html)")
   json <- ctx$get("json", simplifyVector = FALSE)
   out <- html2R:::parse_html(json)
+  out <- paste("htmltools::tagList(\n\t", out,"\n)\n")
   cat(out)
   out <- rlang::parse_expr(out)
   out <- eval(out)
